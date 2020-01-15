20132f089979c9fc5fdef8260b16014af5f6c654
Bishesh Bhattarai <bishesh@localhost.localdomain>
2020-01-15 12:14:33 +0545

    Mulakat nahona na ho lag ja galeee

--- shared_power_102/script.py
+++ shared_power_102/script.py
@@ -9,325 +9,282 @@ COMPLETED:
 	3> Add
 
 '''
-
+from tkinter import *
 import sqlite3
-
+import sys
 conn = sqlite3.connect('user.db')
 c = conn.cursor()
-
+FEATS = ['Register', 'Login', 'View&Search', 'View Cart', 'Add Tool', 'Logout']
+global d
 SESSION = []
 ACTION = []
-
+SYSTEM_ON = False
 #DATABASE INITIALIZATION
 #--------------------------------------------------------------------------------------
-try:
-	J
-	c.execute("""CREATE TABLE users(
-			username text,
-			email text,
-			password text,
-			address text,
-			phoneno integer
-			)""")
-	c.execute("""CREATE TABLE tools(
-			tool_name text,
-			half_day_rate integer,
-			full_day_rate integer,
-			description text
-			)""")
-	c.execute("""CREATE TABLE invoices(
-				description text,
-				quantity integer,
-				price integer,
-				total integer
-			)""")
-except Exception as e:
-	print(e)
-	
-#-------------------------------------------------------------------------------------------------
-#DATABASE COMMANDS
-def register(username, email, password, address , phoneno):
-	c.execute("INSERT INTO users VALUES(?,?,?,?,?)",(username, email, password, address, phoneno))
-	conn.commit()
+class LoginPage:
+
+    def __init__(self):
+        window = Tk()
+        window.geometry('300x500')
+        username_label = Label(window, text="Username : ")
+        username_label.grid(row=0, column=0)
+        username_entry = Entry(window)
+        username_entry.grid(row=0, column=1)
+        password_label = Label(window, text="Password : ")
+        password_label.grid(row=1, column=0)
+        password_entry = Entry(window)
+        password_entry.grid(row=1, column=1)
+        login_button = Button(text="Login", command=lambda : d.login(username_entry.get(), password_entry.get(), window))
+        login_button.grid(sticky='nsew')
+        gap = Label(window)
+        gap.grid(pady=10)
+        down_text = Label(window, text="Not registered yet ? ")
+        down_text.grid(sticky='nsew')
+        down_text_button = Button(text="Register", command=lambda : show_register_page(True, window))
+        down_text_button.grid(sticky='nsew')
+        window.mainloop()
+
+class RegisterPage():
+
+    def __init__(self):
+        window = Tk()
+        window.geometry('300x500')
+        username_label = Label(window, text="Username : ")
+        username_label.grid(row=0, column=0)
+        username_entry = Entry(window)
+        username_entry.grid(row=0, column=1)
+        password_label = Label(window, text="Password : ")
+        password_label.grid(row=1, column=0)
+        password_entry = Entry(window, show='*')
+        password_entry.grid(row=1, column=1)
+        email_label = Label(window, text="Email : ")
+        email_label.grid(row=2, column=0)
+        email_entry = Entry(window)
+        email_entry.grid(row=2, column=1)
+        address_label = Label(window, text="Address : ")
+        address_label.grid(row=3, column=0)
+        address_entry = Entry(window)
+        address_entry.grid(row=3, column=1)
+        phoneno_label = Label(window, text="Phone-no : ")
+        phoneno_label.grid(row=4, column=0)
+        phoneno_entry = Entry(window)
+        phoneno_entry.grid(row=4, column=1) 
+        register_button = Button(text="Register", command=lambda : d.register(username_entry.get(), password_entry.get(), email_entry.get(), address_entry.get(), phoneno_entry.get(), window))
+        register_button.grid(sticky='nsew')
+        window.mainloop()
+
+class AddToolPage():
+
+    def __init__(self):
+        window = Tk()
+        window.geometry('300x500')
+        tools_name_label = Label(window, text="Tool's name ")
+        tools_name_label.grid(row=0, column=0)
+        tools_name_entry = Entry(window)
+        tools_name_entry.grid(row=0, column=1)
+        description_label = Label(window, text="Description : ")
+        description_label.grid(row=1, column=0)
+        description_entry = Entry(window)
+        description_entry.grid(row=1, column=1)
+        half_day_rate_label = Label(window, text="Half day rate : ")
+        half_day_rate_label.grid(row=2, column=0)
+        half_day_rate_entry = Entry(window)
+        half_day_rate_entry.grid(row=2, column=1)
+        full_day_rate_label = Label(window, text="full day rate : ")
+        full_day_rate_label.grid(row=3, column=0)
+        full_day_rate_entry = Entry(window)
+        full_day_rate_entry.grid(row=3, column=1)
+       
+        submit_button = Button(text="Submit", command=lambda : d.add_tool(tools_name_entry.get(), description_entry.get(), half_day_rate_entry.get(), full_day_rate_entry.get(),window))
+        submit_button.grid(sticky='nsew')
+        window.mainloop()
+
+class DashBoard:
+
+    def __init__(self):
+        window = Tk()
+        window.geometry("300x500")
+        title_label = Label(window, text="Welcome "+ SESSION[0].upper()+ " to your dashboard")
+        title_label.pack()
+        gappp = Label(window)
+        gappp.pack(pady=7)
+        search_entry = Entry(window)
+        search_entry.insert(0, 'Search here...')
+        search_entry.pack()
+        search_submit = Button(window, text="Submit", command=lambda : t.view_and_hire(search_entry.get(), window))
+        search_submit.pack()
+        s =  t.get_tools(SESSION[0])
+        counter1 = 0
+        counter2 = 1
+        print(len(s))
+        for i in s:
+            # counter2 += 1
+            # print(len(s))
+           
+            gap = Label(window, text="--------------------------------------")
+            gap.pack(pady=10)
+            for k in i:
+                photo = Label(window, text="#TODO PHOTO GOES HERE")
+                labels = Label(window, text=k)
+                labels.pack()
+                counter1 += 1
+                counter2 += 1
+                # print(counter1, counter2)
+                
+              
+
+def login_page():
+    a = LoginPage()
+
+def show_register_page(p , window):
+    if p == True:
+        window.destroy()
+    b = RegisterPage()
+def show_dashboard():
+    c = DashBoard()
+def add_tool_page():
+    a = AddToolPage()
 
 
-def check_user(username):
-	c.execute("SELECT * FROM users WHERE username = ?", (username,))
-	print(c.fetchall())
-	if len(c.fetchall()) == 0:
-		return True 
+class Db:
+    def __init__(self):
+     
+        try: 
+    
+            c.execute("""CREATE TABLE users(
+            username text,
+            email text,
+            password text,
+            address text,
+            phoneno integer
 
-def validate_login(username, password):
-	c.execute("SELECT * FROM users WHERE username=? and password=?", (username, password))
-	if bool((c.fetchall())):
-		return True
+            )""")
+        except:
+            pass
+        try:
+            c.execute( """CREATE TABLE tools(
+            tool_name text,
+            half_day_rate integer,
+            full_day_rate integer,
+            description text,
+            user_name text,
+            FOREIGN KEY(user_name) REFERENCES users(username)
+            )""")
+        except Exception as e:
+            print(e)
+        try:
+            c.execute(  """CREATE TABLE invoices(
+                description text,
+                quantity integer,
+                price integer,
+                total integer
+            )""")
+        except Exception as e:
+            print(e)
 
-def add_tool(tool_name, half_day_rate, full_day_rate, description):
-	c.execute("INSERT INTO tools VALUES(?, ?, ?, ?)",(tool_name, half_day_rate, full_day_rate, description))
-	conn.commit()
-
-def get_tool(tool_name, description):
-	c.execute("SELECT * FROM tools WHERE tool_name=?", (tool_name,))
-	if len(c.fetchall()) != 0:
-		print(c.fetchall())
-	else:
-		print("No tools available")
-def add_in_session(user):
-	SESSION.append(user)
-
-def check_session_status(username):
-	if len(SESSION) == 1 and SESSION[0] == username:
-		return True
-
-
-
-
-
-#------------------------------------------------------------------
-#Code for validating proper and strong password
-
-def check_alphanumeric_count(a):
-    chars = ['@', '$', '%', '#', '&', '"', '!', '*', '^', "'"]
-    s = []
-    for i in a:
-        for j in chars:
-            if i == j:
-                s.append(True)
-    if len(s) >= 2:
-        return True
-    else:
-        return False
-
-
-def check_num_count(a):
-    num = [(i+1) for i in range(10)] # [1, 2 , 3, 4, 5 ,6, 7, 8, 9, 10]
-    s = []
-    for i in a:
-        for n in num:
-            if i == str(n):
-                s.append(True)
-    if len(s) >= 2:
-        return True
-    else:
-        return False        
-
-
-def check_password_strength(password):
-    if len(password) >= 7:
-        if check_alphanumeric_count(password) and check_num_count(password):
+    def get_one_user(self,username):
+        c.execute("""SELECT * FROM users WHERE username=?""",(username,))
+        a = c.fetchall()
+        if len(a) == 0:
             return True
+
+    def register(self,username,  password ,email, address , phoneno, controller):
+   
+        print(username, password ,email)
+        if username != '' and password != '' and email != '' and address != '' and phoneno != 0:
+            if self.get_one_user(username):
+                command = """INSERT INTO users VALUES(? , ? , ? , ? ,? )"""
+                c.execute(command, (username, email, password, address, phoneno))
+                conn.commit()
+                controller.destroy()
+            else:
+                print('User exists already with same name')
         else:
-            print("Password error : More alphanumeric or num req")
-            return False
-    else:
-        print("Password error : Too less characters : MIN required")
-        return False
-#---------------------------------------------------------------
-class Registration:
-	def __init__(self):
-		self.username = ''
-		self.password = ''
-		self.email = ''
-		self.address = ''
-		self.phoneno = 0
+            print('All fields is necessary')
 
-	def check_login(self, username='', password='', email='', address='', phoneno=0):
-		if username and password:
-			return True
-	
-	
-	def login(self):
-		print("\n-----------------\nLOGIN\n-----------------")
-		self.username = input("Username : ")
-		self.password = input("Password : ")
-		if self.check_login(self.username, self.password):
-			if validate_login(self.username, self.password):
-				SESSION.append(self.username)
-				return True
-			else:
-				print("Wrong credentials")
-		else:
-			self.register()
+    def validate_user(self, username, password):
+        c.execute("""SELECT * FROM users WHERE username=?""",(username,))
+        s = c.fetchall()
+        if len(s) != 0:
+            if s[0][2] == password:
+                return True
 
-	def logout(self):
-		SESSION.pop()
-				 
-	def check_register(self, username='', password='', email='', address='', phoneno=0):
-		if username and password and len(password) >=7 and address and phoneno and type(phoneno) == type(1):
-			return True
+    def login(self, username, password, controller):
+        if username != '' and password != '':
+            if self.validate_user(username, password):
+                SESSION.append(username)           
+                print("logged in")
+                print(SESSION)
+                controller.destroy()
+                show_dashboard()
+                
+            else:
+                print("Incorrect password")
+        else:
+            print("All fields needed")
 
-	def register(self):
-		
-		
-		print("\n-----------------\nREGISTER\n-----------------")
-		self.username = input("Username : ")
-		self.password = input("Password : ")
-		self.address = input("Address : ")
-		self.email = input("Email : ")
-		self.phoneno = int(input("Phoneno : "))
-		if self.check_register(self.username, self.password , self.email, self.address, self.phoneno):
-			if check_user(self.username):
-				if check_password_strength(self.password):
-					register(self.username, self.email, self.password , self.address, self.phoneno)	
-					print("User registered")
-				
-			else:
-				print("User with same name already exists.")
-		else:
-			print("Error")
+    def add_tool(self,tool_name, description , half_day_rate, full_day_rate, window):
+        if tool_name != '' and description != '' and half_day_rate != '' and full_day_rate != '':
+            command = """INSERT INTO tools VALUES(?,?,?,?,?)"""
+            c.execute(command, (tool_name, description, half_day_rate, full_day_rate, SESSION[0]))
+            conn.commit()
+            print("Tools added")
+            window.destroy()
+            #GOTODASHBOARD
+d = Db()
 
-#-----------------------------------------------------------------------------------------------------
+class Hire:
 
-		
+    def __init__(self, tool_name, s):
+        window = Tk()
+        window.geometry("300x500")
+        # label = Label(window, text=tool_name)
+        # label.grid(row=0, column=0)
+        c.execute("""SELECT * FROM tools WHERE tool_name=?""", (tool_name,))
+        a = c.fetchall()
+        description_label = Label(window , text='Description : ')
+        description_label.grid(row=0, column=0)
+        half_day_rate_label = Label(window, text='Half day rate : ')
+        half_day_rate_label.grid(row=1, column=0)
+        full_day_rate_label = Label(window, text='Full day rate : ')
+        full_day_rate_label.grid(row=2, column=0)
+        owner_label = Label(window, text='Owner : ')
+        owner_label.grid(row=3 , column=0)
+        description = Label(window , text=a[0][1])
+        description.grid(row=0, column=1)
+        half_day_rate = Label(window, text=a[0][2])
+        half_day_rate.grid(row=1, column=1)
+        full_day_rate = Label(window, text=a[0][3])
+        full_day_rate.grid(row=2, column=1)
+        owner = Label(window, text=a[0][4])
+        owner.grid(row=3 , column=1)
+        window.mainloop()
+        
 
-class Tools(Registration):
+def hireandviewPage(tool_name, window, s):
+    window.destroy()
+    h = Hire(tool_name, s)    
 
-	def __init__(self):
-		super().__init__()
-		self.tool_name = ''
-		self.description = ''
-		self.half_day_rate = 0
-		self.full_day_rate = 0 
+class Tools:
 
-	def get_tool_name(self):
-		self.tool_name = input("Enter tool name")
+    def get_tools(self, username):
+        c.execute("""SELECT * FROM tools WHERE user_name != ?""", (username,))
+        return c.fetchall()
 
-	def search_tool(self):
-		if check_session_status(self.username):
-			self.get_tool_name()
-			if self.get_tool_name is not None:
-				get_tool(self.tool_name)
-			else:
-				print("Please enter tool name")
+    def view_and_hire(self, tool_name, window):
+        c.execute("""SELECT * FROM tools WHERE tool_name == ?""", (tool_name,))
+        if len(c.fetchall()) != 0:
+            hireandviewPage(tool_name, window, True)
+        else:
+            # hireandviewPage(tool_name , window, False)
+            print("No such items")
+    # def get_one_tool()
+    
+t = Tools()
 
-	def set_tool_data(self):
-		self.tool_name = input("Enter tool name")
-		self.description = input("Enter tool description")
-		self.full_day_rate = int(input("Enter full day rate"))
-		self.half_day_rate = int(input("Enter half day rate"))
-
-	def add_tool(self, tool_name, half_day_rate, full_day_rate, description):
-		if check_session_status(self.username):
-			self.set_tool_data()
-			try:
-				add_tool(self.tool_name, self.half_day_rate, self.full_day_rate, self.description)
-			except Exception:
-				print(Exception)
-
-#---------------------------------------------------------------------------------------------------------------
-class Invoice(Tools):
-
-	def __init__(self , invoice_no):
-		super().__init__()
-		self.__invoice_no = invoice_no
-		self.description = ''
-		self.quantity = 0
-		self.price = 0
-		self.total = 0
-
-	def generate_invoice(self):
-		pass
 
 
 #----------------------------------------------------------------------------------------------------------------
+#GUI code ..
 
-
-class HomePage:
-
-	def __init__(self):
-		self.feat = []
-		self.action = ''
-		self.username = ''
-		self.password = ''
-		self.email = ''
-		self.address = ''
-		self.phoneno = 0
-
-	def show(self):
-		return "Welcome to shared power\n\n\n\n -----------------------------\n"
-
-	def options(self):
-		self.feat = ['Register', 'Login']
-		j = 0
-		while j<= 1:
-			print("{} : ({})".format(self.feat[j], j))
-			j += 1
-
-
-	def getAction(self, selected=False):
-		s = input("Please enter the action : ")
-		ACTION.append(s)
-		while not bool(ACTION):
-			print("Please give the action index..")
-			s= input("Please enter the action : ")
-		print(ACTION)
-
-class Index(Tools):
-
-	def __init__(self):
-		super().__init__()
-		self.feats = ['Add Tool', 'View & search tool', 'View Cart', 'Logout']
-
-
-	def show(self):
-		j = 2
-		print("\n\n--------------------------------------Welcome ! {} , to shared power------------------------------------------------------------".format(SESSION[0]))
-
-		for i in self.feats:
-			print("\n\n{} : ({})".format(i, j))
-			j += 1
-class Distributer(Index , HomePage, Registration, Tools):
-
-	def __init__(self):
-		super().__init__()
-		pass
-	
-	def distribute(self):
-		if ACTION[0] == '1':
-			if self.login():
-				self.show()
-				ACTION.pop()
-				self.getAction()
-				if len(ACTION) == 1:
-					self.distribute()
-
-		elif ACTION[0] == '0':
-			self.register()
-		elif ACTION[0] == '2':
-			print(SESSION)
-		elif ACTION[0] == '3':
-			self.tool
-		elif ACTION[0] == '4':
-			print(SESSION)
-		elif ACTION[0] == '5':
-			print(SESSION)
-
-class Delivery(Tools, Invoice, Registration):
-
-	def __init__(self):
-		super().__init__()
-		self.total_deliveries = 6
-		self.available = 6
-		if self.out_of_valley:
-			self.delivery_open = True
-		self.delivery_open = False
-		
-	def delivery(self):
-		if self.delivery_open and bool(self.available):
-			self.available -= 1
-	
-	def update_invoice():
-		pass
-
-class Insurance(Tools, Registration):
-
-	def __init__(self):
-		super().__init__()
-		pass
-
-i = HomePage()
-print(i.show())
-i.options()
-i.getAction()
-d = Distributer()
-d.distribute()
+login_page()
