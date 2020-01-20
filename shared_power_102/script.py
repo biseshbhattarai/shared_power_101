9afc11c0aa0221874679e229970a6f475a8e2fbd
Bishesh Bhattarai <bishesh@localhost.localdomain>
2020-01-20 11:31:15 +0545

    update added

--- shared_power_102/script.py
+++ shared_power_102/script.py
@@ -1,20 +1,11 @@
 #! /usr/bin/python3
 
 #PROJECT STATUS
-    
-'''
-COMPLETED:
-	1> Register
-	2> Login
-	3> Add
-    4> Add to cart
-    3> Dashboard page
-    
-
-'''
 from tkinter import *
 import sqlite3
 import sys
+from tkinter import messagebox as msg
+import datetime
 conn = sqlite3.connect('user.db')
 c = conn.cursor()
 SESSION = []
@@ -70,7 +61,9 @@ class RegisterPage():
         phoneno_entry = Entry(window)
         phoneno_entry.grid(row=4, column=1) 
         register_button = Button(text="Register", command=lambda : d.register(username_entry.get(), password_entry.get(), email_entry.get(), address_entry.get(), phoneno_entry.get(), window))
-        register_button.grid(sticky='nsew')
+        register_button.grid(row=6 , column=2)
+        back = Button(text="Back", command=lambda:back_login(window))
+        back.grid(row=8, column=2)
         window.mainloop()
 
 class AddToolPage():
@@ -98,11 +91,17 @@ class AddToolPage():
         submit_button = Button(text="Submit", command=lambda : d.add_tool(tools_name_entry.get(), description_entry.get(), half_day_rate_entry.get(), full_day_rate_entry.get(),window))
         submit_button.grid(sticky='nsew')
         window.mainloop()
-
+           
+def check(s):
+    print(s)
 class DashBoard:
 
     def __init__(self):
         window = Tk()
+        scrollbar = Scrollbar(window)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        listbox = Listbox(window, yscrollcommand=scrollbar.set)
         window.geometry("300x500")
         title_label = Label(window, text="Welcome "+ SESSION[0].upper()+ " to your dashboard")
         title_label.pack()
@@ -116,31 +115,26 @@ class DashBoard:
         search_entry.insert(0, 'Search here...')
         search_entry.pack()
         search_submit = Button(window, text="Submit", command=lambda : t.view_and_hire(search_entry.get(), window))
-        search_submit.pack()
+        search_submit.pack(pady=15)
+        Label(window, text="AVAILABLE TOOLS").pack(pady=12)
+
         s =  t.get_tools(SESSION[0])
-        counter1 = 0
-        counter2 = 1
+       
         print(len(s))
+
         for i in s:
-            # counter2 += 1
-            # print(len(s))
-           
-            gap = Label(window, text="--------------------------------------")
-            gap.pack(pady=10)
-            for k in i:
-                photo = Label(window, text="#TODO PHOTO GOES HERE")
-                labels = Label(window, text=k)
-                labels.pack()
-                counter1 += 1
-                counter2 += 1
-                # print(counter1, counter2)
+           listbox.insert(END, i[0])
+        listbox.pack(side=LEFT, fill=BOTH)
+        scrollbar.config(command=listbox.yview)
+        window.mainloop()
+
                 
 class MyToolsPage:
 
     def __init__(self):
         window = Tk()
         window.geometry("300x500")
-        c.execute("""SELECT * FROM tools WHERE user_name=?""",(SESSION[0],))
+        c.execute("""SELECT * FROM hirings WHERE username=?""",(SESSION[0],))
         a = c.fetchall()
         for i in a:
             label = Label(window, text=i)
@@ -152,6 +146,9 @@ def my_tools_page(window):
     m = MyToolsPage()
 
 def login_page():
+    a = LoginPage()
+def back_login(window):
+    window.destroy()
     a = LoginPage()
 
 def show_register_page(p , window):
@@ -214,7 +211,22 @@ class Db:
             """)
         except Exception as e:
             print(e)
-
+        try:
+            c.execute("""
+                    CREATE TABLE hirings(
+                        username text,
+                        tool_name text,
+                        hired_date timestamp,
+                        quantity integer,
+                        delivery text,
+                        FOREIGN KEY(username) REFERENCES users(username),
+                        FOREIGN KEY (tool_name) REFERENCES tools(tool_name)
+                        
+                    )
+            
+            """)
+        except :
+            pass
     def get_one_user(self,username):
         c.execute("""SELECT * FROM users WHERE username=?""",(username,))
         a = c.fetchall()
@@ -230,17 +242,20 @@ class Db:
                 c.execute(command, (username, email, password, address, phoneno))
                 conn.commit()
                 controller.destroy()
+                login_page()
             else:
-                print('User exists already with same name')
+                msg.showerror('Error', 'Username with same name already exists')
         else:
-            print('All fields is necessary')
+            msg.showerror('Error', 'Please fill all the fields')
 
     def validate_user(self, username, password):
         c.execute("""SELECT * FROM users WHERE username=?""",(username,))
         s = c.fetchall()
         if len(s) != 0:
-            if s[0][2] == password:
-                return True
+            if len(s) != 0:
+                if s[0][2] == password:
+                    return True
+        msg.showerror('Error', 'User doesnt exist')
 
     def login(self, username, password, controller):
         if username != '' and password != '':
@@ -252,16 +267,16 @@ class Db:
                 show_dashboard()
                 
             else:
-                print("Incorrect password")
+                msg.showerror('Error', 'Wrong password')
         else:
-            print("All fields needed")
+            msg.showerror('Error', 'All fields are necessary')
 
     def add_tool(self,tool_name, description , half_day_rate, full_day_rate, window):
         if tool_name != '' and description != '' and half_day_rate != '' and full_day_rate != '':
             command = """INSERT INTO tools VALUES(?,?,?,?,?)"""
             c.execute(command, (tool_name, description, half_day_rate, full_day_rate, SESSION[0]))
             conn.commit()
-            print("Tools added")
+            msg.showinfo('Success', 'Tool is added')
             window.destroy()
             #GOTODASHBOARD
 
@@ -296,6 +311,10 @@ class Hire:
         add_to_cart_button = Button(window, text="Add to cart" , 
         command=lambda : t.add_to_cart(tool_name, SESSION[0], window))
         add_to_cart_button.grid(row=4, column=3)
+        data = []
+        data.append((SESSION[0], tool_name, datetime.datetime.now().strftime("%Y%m%d")))
+        hire_button = Button(window, text="Hire", command=lambda: t.confirm_hire(data, window))
+        hire_button.grid(row=5, column=3)
         window.mainloop()
         
 
@@ -315,22 +334,79 @@ class Tools:
             hireandviewPage(tool_name, window, True)
         else:
             # hireandviewPage(tool_name , window, False)
-            print("No such items")
+            msg.showerror('Error', 'No such items found')
+            # print("No such items")
     # def get_one_tool()
     
     def add_to_cart(self , tool_name, username, window):
-        window.destroy()
         c.execute("""INSERT INTO cart VALUES(? , ? )""", (username , tool_name))
-        c.commit()
-        print("Added to cart")
+        conn.commit()
+        msg.showinfo('Success', tool_name + ' is added on cart')
+        window.destroy()
         show_dashboard()
 
     #HERE WE"LL UPDATE THE INVOICE 
-    def hire_tools(self, cart_data, window):
-        for i in cart_data:
-            print("{} are hired".format(i))
+    def hire_tools(self, cart_data, quantity,delivery,window):
+        print(cart_data)
+        if delivery == 1:
+            s = 'True'
+        else:
+            s = 'False'
+        if len(cart_data) > 1:
+            for i in cart_data:
+                print("{} are hired".format(i[1]))
+                c.execute("""INSERT INTO hirings VALUES(?, ?, ?, ? , ?)""", (i[0], i[1], 
+                datetime.datetime.now().strftime("%Y%m%d"), quantity , s))
+                conn.commit()
+        else:
+            print("{} is hired".format(cart_data))
+            c.execute("""INSERT INTO hirings VALUES (?, ?, ? , ? , ?)""", (cart_data[0][0], cart_data[0][1], 
+        datetime.datetime.now().strftime("%Y%m%d"), quantity, s))
+            conn.commit()
+        c.execute("""DELETE FROM cart WHERE username=?""", (SESSION[0], ))
+        conn.commit()
+        msg.showinfo('showinfo ', 'Above item/s are hired')
+        print("Cart data of {} is deleted".format(SESSION[0]))
         window.destroy()
         show_dashboard()
+
+    def confirm_hire(self, data, window):
+        window.destroy()
+        window = Tk()
+        window.geometry("300x500")
+        quantity_label = Label(window, text="Quantity")
+        quantity_label.pack()
+        quantity = Entry(window)
+        quantity.insert(0, 1)
+        quantity.pack()
+        delivery = IntVar()
+        delivery_opt = Checkbutton(window, variable=delivery , text="Request for delivery")
+        delivery_opt.pack()
+        Button(window, text="Confirm" , command=lambda: t.hire_tools(data, quantity.get(),delivery, window)).pack()
+        # t.hire_tools(data, quantity.get(), delivery_opt.get(), window)
+
+    def generate_invoice(self):
+        c.execute("""SELECT * FROM hirings WHERE username=?""", (SESSION[0]))
+        a = c.fetchall()
+        today_date = datetime.datetime.now().strftime("%Y%m%d")
+        late = []
+        for i in a:
+            if (today_date - i[2]) > 3:
+                late.append(True)
+            
+        total_fine = len(late) * 50
+        ext = 0
+        s = []
+        for i in a:
+            if i[4] == 'True':
+                ext = 50
+            s.append(i[1]+"\t"+i[2]+"\t"+i[3]+"\t"+total_fine+"\t"+ext)
+        return s
+
+    def return_tools(self):
+        a = self.generate_invoice()
+        view_invoice_page(a , window)
+
 
 
 def get_cart_data():
@@ -348,9 +424,19 @@ class CartPage:
         for i in cart_data:
             label = Label(window, text=i[1])
             label.pack()
-        hire_button = Button(window, text="Hire" , command=lambda : t.hire_tools(cart_data, window))    
+        if len(cart_data) != 0:
+            hire_button = Button(window, text="Hire" , command=lambda : t.confirm_hire(cart_data, window))    
+            hire_button.pack()
         button = Button(window ,text="Back", command=lambda: show_dashboard_(window))
         button.pack()
+
+class Invoice:
+
+    def __init__(self, data):
+        window = Tk()
+        window.geometry("300x500")
+
+
 
 #CALLING TOOL INSTANCE
 
